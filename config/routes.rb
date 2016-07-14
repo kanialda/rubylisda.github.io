18f76cdd7f3ae339962a6bb63a86acfddf16d1ee
Lisda Kania Yuliani <kanialisda@gmail.com>
2016-07-14 15:04:01 +0700

    Implement bootstrap with RoR

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'profiles/index'
+
+  get 'contacts/index'
+
+  get 'homes/index'
+
+  get 'profile/index'
+
+  get 'contact/index'
+
+  get 'home/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
